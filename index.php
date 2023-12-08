8c069903035fe023565bb16fe9f937882fe89225
Chroust25 <rvoderka@gmail.com>
2023-12-08 13:02:50 +0100

    .

--- index.php
+++ index.php
@@ -2,7 +2,15 @@
 require 'vendor/autoload.php';  //pokud jedu přes composer
 $f3 = \Base::instance();        //pokud jedu přes composer
 $f3->config("app/configs/config.ini");
-$f3->set('DB', new \DB\SQL('mysql:host=localhost;dbname=skaut','root', ''));
+
+//$f3->set('DB', new \DB\SQL('mysql:host=;dbname=skaut','root', ''));
+
+$f3->set('DB', new \DB\SQL(
+    $f3->get('db.dsn'),
+    $f3->get('db.username'),
+    $f3->get('db.password')
+));
+
 $f3->run();
 
 
